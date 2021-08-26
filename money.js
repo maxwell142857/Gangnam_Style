2bb46fde4cafacc4cef36ba1e8e730dad357ec76
maxwell142857 <1587912667@qq.com>
2021-08-26 11:36:12 +0800

    修改了说明文件

--- money.js
+++ money.js
@@ -1,11 +1,18 @@
 //2312*1080
 //在横屏模式下，左上角为原点，水平方向为x轴,竖直方向为y轴建系
+//程序分为三步，点击水井，选择水井类型，选择人物（请确保至少有一个特殊居民，不然不会出现选择人物的界面）
+//根据大量测试，只会在第三步出现人机验证，故只在第三步执行了人机验证检测的逻辑
+
+//以下信息请根据自身情况修改
 var your_x = 2312;//填写手机像素比较大的那个值
 var your_y = 1080;//填写手机像素比较小的那个值
+var well_cnt = 20;//填写在样图的布置下，使用的水井数（因为有时手上工人不到30个），上限为20
+
+//除非充分理解程序，否则不要修改以下信息
 var x_ratio = your_x / 2312;
 var y_ratio = your_y / 1080;
 var REBACK = [2000*x_ratio, 700*y_ratio]; // 无效点，地图上一个没有建筑的点，用于返回操作
-var SLEEP = 500; // 休眠（ms），一般不用动
+var SLEEP = 500; // 休眠（ms）
 var MINS = 2; // 循环用时（min）
 var WARN = 3; // 提醒时间（s）
 var TASKS = [
@@ -18,32 +25,12 @@ var TASKS = [
     [680*x_ratio, 700*y_ratio], [1090*x_ratio, 700*y_ratio], [1490*x_ratio, 700*y_ratio],
     [897*x_ratio, 800*y_ratio],
     [1110*x_ratio, 900*y_ratio],
-];
+];//水井的位置
 var jigsaw_x = [880*x_ratio,1010*x_ratio,1140*x_ratio,1270*x_ratio,1400*x_ratio,1530*x_ratio];//拼图的x轴坐标
 var jigsaw_y =[520*y_ratio,640*y_ratio];//拼图的y坐标
 var mywell = images.read("./resource/well.jpg");
 var myjigsaw = images.read("./resource/jigsaw.jpg");
 
-//确保进入了选择水井类型界面
-function click_well(x, y) {
-    click(x, y);
-    sleep(SLEEP);
-    check_jigsaw();
-    var cnt = 0;
-    while (true) {
-        if (check_well()) {
-            break;
-        } else {
-            click(REBACK[0], REBACK[1]);//返回
-            sleep(SLEEP);
-            click(x, y);//再点一次
-            sleep(SLEEP);
-            check_jigsaw();
-            cnt++;
-            console.assert(cnt < 10, "多次发生点击异常，已退出")
-        }
-    }
-}
 
 //判断是否处于水井页
 function check_well() {
@@ -106,24 +93,38 @@ function jigsaw(x, y) {
 function main(task) {
     toast("将于" + WARN + "秒后开始");
     sleep(WARN * 1000);
-    for (var i in task) {
-
-        click_well(TASKS[i][0], TASKS[i][1]);//点击水井，进入水井模式选择页面
+    var fail_cnt = 0;//每个轮回中失败的井的个数，偶尔有一两个正常（点到人了），如果过多，说明脚本已经不是在预期的环境下执行了，终止脚本
+    for(var i = 0;i<well_cnt;i++){
+        click(TASKS[i][0], TASKS[i][1]);//点击水井，进入水井模式选择页面
+        sleep(SLEEP);
+        if(!check_well()){
+            click(REBACK[0], REBACK[1]);//返回
+            sleep(SLEEP);
+            click(TASKS[i][0], TASKS[i][1]);//再点一次
+            sleep(SLEEP);
+            if(!check_well()){
+                click(REBACK[0], REBACK[1]);//返回
+                sleep(SLEEP);
+                fail_cnt++;
+                log("选择" + i + "号水井时失败，已进入下一个水井选择");
+                continue;
+            }
+        }
         log("已选择" + i + "水井");
         sleep(SLEEP);
 
         click(1000*x_ratio, 260*y_ratio);//选择最低级水井，进入人员选择
         sleep(SLEEP);
-        check_jigsaw();
-        sleep(SLEEP);
         log("选择最低级水井")
 
         click(1000*x_ratio, 260*y_ratio);//选择普通居民
-        sleep(SLEEP);
+        sleep(SLEEP*2);
         check_jigsaw();
         sleep(SLEEP);
         log("选择普通居民")
     }
+    log("总失败次数:" + fail_cnt);
+    console.assert(fail_cnt*2<well_cnt, "多次发生点击异常，已退出");
 }
 
 
